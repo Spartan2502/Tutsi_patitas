292586876f0df89ed75e68e9c787719488d0a208
Alex Orbe / Spartan2502 <josealexorbech@hotmail.com>
2022-05-05 18:53:49 -0500

    First Comit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,8 @@
+
+export default function Formulario() {
+    return (
+        <div className="">
+            <h2>Formulario</h2>
+        </div>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <>
+        <h1>Desde Header</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde dolores exercitationem accusantium repudiandae similique laboriosam atque recusandae, iure quas aliquam aspernatur ipsa sapiente delectus distinctio? Voluptate ratione quod tempore quae!</p>
+        </>
+    )
+
+
+}
+export default Header
