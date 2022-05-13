9e8bfcf058db4f9870e9d4f28f8a9c9372f7a9d2
Spartan2502/AlexOrbe <josealexorbech@hotmail.com>
2022-05-12 23:00:37 -0500

    final configs

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -43,7 +43,7 @@ const Paciente = ({ paciente, setPaciente, Eliminar }) => {
 
 
 
-                <button type="button" className="inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out" onClick={handleEliminar}>
+                <button type="button" className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg" onClick={handleEliminar}>
                     Eliminar
                 </button>
             </div>
